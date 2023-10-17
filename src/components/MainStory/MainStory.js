d59ff3ec5b28840115fc294910e1acc9817ef5f1
alunturner <alun.turner@googlemail.com>
2023-10-17 12:53:37 +0100

    add some truncation

--- src/components/MainStory/MainStory.js
+++ src/components/MainStory/MainStory.js
@@ -43,7 +43,10 @@ const Heading = styled.h2`
 const Abstract = styled.p`
   font-size: 1rem;
   margin-bottom: 1em;
-  white-space: pre-wrap;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 8;
+    overflow: hidden;
 `;
 
 const Location = styled.span`
